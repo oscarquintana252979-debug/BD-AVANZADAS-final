234216330c6558a34aed223a34869d076eec650b
Oscar <oscar.quintana252979@potros.itson.edu.mx>
2026-07-08 09:15:57 -0700

    Estructura Maven y clases de dominio

---
+++ src/main/java/com/equipo4/bibliotecamusical/BibliotecaMusical.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.equipo4.bibliotecamusical;
+
+/**
+ *
+ * @author oscar
+ */
+public class BibliotecaMusical {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
